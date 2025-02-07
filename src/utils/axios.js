434d7f70ef0331fdff6ac032bfa56d0d8ac21496
bansalKrishna311 <bansalKrishna311@gmail.com>
2025-02-07 23:35:05 +0530

    fix: update production API URL in axios instance

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
 	baseURL: import.meta.env.MODE === "development" 
 		? "http://localhost:5000/api/" // Use your local development URL here
-		: "https://bharat-techx.vercel.app/api", // Use your production URL here
+		: "https://bharat-tech-backend.vercel.app/api", // Use your production URL here
 	withCredentials: false,
 });
